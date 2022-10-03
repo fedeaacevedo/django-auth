84151f345383d4722ecb34a17a484cf3c45e83c5
Federico <fedeaacevedo@gmail.com>
2022-10-02 22:19:23 -0300

    base & forms

--- tasks/views.py
+++ tasks/views.py
@@ -1,14 +1,15 @@
+from django.db import IntegrityError
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-
+from django.contrib.auth import login, logout
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 
-
+# Esta vista nos permite guardar datos de un usuario
 def signup(request):
     # metodo get devolver a la pagina del formulario
     if request.method == 'GET':
@@ -21,10 +22,25 @@ def signup(request):
             # Registramos usuarios con los modelos de django
             try:
                 user = User.objects.create_user(
-                    username=request.POST['username'], password=request.POST['password1'])
+                username=request.POST['username'], password=request.POST['password1'])
                 user.save()
-                return HttpResponse('Usuario creado exitosamente')
+                # usamos login para que nos guarde la sesion y nos cree cookies
+                login(request, user)
+                return redirect('tasks')
 
-            except:
-                return HttpResponse('El usuario ya existe')
-        return HttpResponse('Contraseñas no coinciden')
+            except IntegrityError:
+                 return render(request, 'signup.html', {
+                    'form': UserCreationForm,
+                    'error': 'El usuario ya existe'
+                })    
+        return render(request, 'signup.html', {
+                    'form': UserCreationForm,
+                    'error': 'Las contraseñas deben coincidir'
+                }) 
+
+def tasks(request):
+    return render(request, 'tasks.html')
+
+def signout(request):
+    logout(request)
+    return redirect('home')
